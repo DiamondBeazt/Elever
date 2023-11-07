79ea1a305f580994800caf255fa759a4f870eef8
DiamondBeazt <142757711+DiamondBeazt@users.noreply.github.com>
2023-11-07 22:59:07 +0100

    Add files via upload

--- Elever.js
+++ Elever.js
@@ -1,7 +1,10 @@
-let y = 10; let r = 200; g = 180; b = 0;
+let y = 10; let x = 10; let r = 200; g = 180; b = 0;
 let ned = true;
 let rystet = 0;
 let flyttet = 0;
+let xDirection = 0;
+let yDirection = 1;
+
 
 function setup() {
     canvas = createCanvas(500, 800, 'beholder');
@@ -56,66 +59,57 @@ function draw() {
     let rek4 = {x: 260, y: 80, w:150, h:6};
     let rek5 = {x: 150, y: 265, w:180, h:6};
     let rek6 = {x: 335, y: 170, w:6, h:101};
-    let rek7 = {x: 410, y: 170, w:6, h:101};
-    let rek8 = {x: 70, y: 220, w:6, h:101};
-    let rek9 = {x: 70, y: 430, w:6, h:101};
-    let rek10 = {x: 420, y: 430, w:6, h:101};
-    let rek11 = {x: 5, y: 430, w:6, h:101};
-    let rek12 = {x: 165, y: 511, w:6, h:101};
-    let rek13 = {x: 80, y: 570, w:6, h:101};
-    let rek14 = {x: 250, y: 570, w:6, h:101};
-    let rek15 = {x: 95, y: 496, w:6, h:101};
-    let rek16 = {x: 95, y: 496, w:6, h:101};
-    let rek17 = {x: 95, y: 170, w:6, h:101};
-    let rek18 = {x: 350, y: 170, w:6, h:101};
-    let rek19 = {x: 430, y: 170, w:6, h:101};
-
 
     let myRectangles = [];
     myRectangles.push(rek1, rek2, rek3, rek4, rek5, rek6);
-    let rek = myRectangles.push;
+    //let rek = myRectangles.push;
     console.log(myRectangles);
 
-    let range = "<ul>";
-    for (let i = 0; i < rek; i++){
-        range += "<li>" + myRectangles[i] + "</li>";
+    for (let i = 0; i < myRectangles.length; i++){
+        intersects(myRectangles[i]);
     }
-    range += "</ul>";
 
     // loop over array - og spærg hver firkant om den rammer dig
-    
-    rect(width/6, y, 10);
-    if (ned)
-        y++;
-    else
-        y--;
-    if (y >= height || y <= 0)
-        ned = !ned;
-    if (accelerationX > 70) {
-        r = random(0, 256);
-        g = random(0, 256);
-        b = random(0, 256);
-        if(rystet%2 == 0)
-        ned = !ned;
-        rystet++;
-        
-    }
-    
-text('rystet: ' + str(rystet), 50, height-100);
-text('flyttet: ' + str(flyttet),50, height-50);
+    x += xDirection;
+    y += yDirection;
+    rect(x, y, 10);
+    // if (accelerationX > 70) {
+
 }
+    
+
+
 
 function deviceMoved(){
-    flyttet++;
+
+
+}
+
+function gyro(){
+    let sensor = new Gyroscope();
+    let x, y, report;
+    sensor.start();
+    sensor.onreading = () => {
+        report = "X-axis Angle Velocity: " + sensor.x + "<br>";
+        report += "Y-axis Angle Velocity: " + sensor.y + "<br>";
+        report += "Ball current X = " + ball.offsetLeft + "<br>";
+        report += "Ball current Y = " + ball.offsetTop + "<br>";
+        report_box.innerHTML = report;
+        x = sensor.x * 100;
+        y = senosr.y * 100;
+        ball.style.left = (ball.offsetLeft + x) + "px"
+        ball.style.top = (ball.offsetTop - y) + "px"
+    };
 
 }
 
 function intersects(range) {
-    if (range.x + range.w < x || range.x  > x + w || range.y + range.h < y || range.y > y +h)
+    console.log(range);
+    if (range.x + range.w < x || range.x  > x + 10 || range.y + range.h < y || range.y > y + 10)
       return false;
     else if(yDirection < 3 && xDirection <3){
-      xDirection +=0.5;
-      yDirection +=0.5;
+      yDirection *= -1;   // +=0.5;
+      //yDirection +=0.5;
       return true;
     }
     else{
